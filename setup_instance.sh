3a0f54910d0109fc7924bb7722a603ec84d0fa3c
Adriaan Dokter <adriaandokter@gmail.com>
2017-10-23 19:03:43 -0400

    fix for crontab statement

--- setup_instance.sh
+++ setup_instance.sh
@@ -6,5 +6,5 @@ aws configure # provide keys
 # log out and back in to get permissions right
 crontab -e
 # for example:
-# 0,10,20,30,40,50 0-13,22,23 * * * /home/ec2-user/mosaic.sh >/dev/null 2>&1
+# 0,10,20,30,40,50 0-13,22,23 * * * root /home/ec2-user/mosaic.sh >/dev/null 2>&1
 # note that a newline is required at the end of the statement
