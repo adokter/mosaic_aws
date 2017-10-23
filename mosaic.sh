b3cc7c67930582de322fc83b351cf62094e96c87
Adriaan Dokter <adriaandokter@gmail.com>
2017-10-23 16:41:18 -0400

    updated launch scripts

--- mosaic.sh
+++ mosaic.sh
@@ -3,4 +3,8 @@
 # do some selection on date
 DATE=`date --date="20 minutes ago" +"%Y-%m-%d %H:%M"`
 
-docker run -ti --rm --env MOSAIC_DATE="$DATE" --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID adokter/mosaic_aws Rscript /opt/mosaic_aws.R
+ID=`sed -n 2p ~/.aws/credentials | awk '{print $3}'`
+KEY=`sed -n 3p ~/.aws/credentials | awk '{print $3}'`
+
+echo $ID
+docker run -ti --rm --env MOSAIC_DATE="$DATE" --env AWS_SECRET_ACCESS_KEY="$KEY" --env AWS_ACCESS_KEY_ID="$ID" adokter/mosaic_aws Rscript /opt/mosaic_aws.R
